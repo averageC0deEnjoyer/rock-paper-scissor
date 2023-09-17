34d1a51ea8e9d42f83c3f31388ada0690939e789
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2023-09-17 15:24:15 +0700

    added some function and refactor the before code

--- script.js
+++ script.js
@@ -2,66 +2,107 @@ let playerScore = 0;
 let compScore = 0;
 
 function getComputerChoice(){
-    randomNum = Math.floor(Math.random()*3) + 1;
-    let compResult = ""
-    if(randomNum == 1){
-        compResult = "rock";
-    } else if(randomNum == 2){
-        compResult = "paper"
-    } else {
-        compResult = "scissor"
-    }
-    return compResult;
+    const choices = ['Rock', 'Paper', 'Scissor'];
+    const randomNum = Math.floor(Math.random()*3);
+    return choices[randomNum];
 };
 
 
-function playRound(){
-    playerSelection = prompt('what your pick r/p/s');
+function playRound(playerSelection){
     compSelection = getComputerChoice();
-    if(playerSelection == compSelection){
-    console.log('its a tie');
-    console.log(`player score: ${playerScore} computer score:${compScore}`)
-    } else if (playerSelection == 'rock' && compSelection == 'scissor' ||
-              playerSelection == 'scissor' && compSelection == 'paper' ||
-              playerSelection == 'paper' && compSelection == 'rock'){
-    console.log(`player win, ${playerSelection} beats ${compSelection}`)
-    playerScore++;
-    console.log(`player score: ${playerScore} computer score:${compScore}`);
-    } else if (compSelection == 'rock' && playerSelection == 'scissor' ||
-              compSelection == 'scissor' && playerSelection == 'paper' ||
-              compSelection == 'paper' && playerSelection == 'rock'){
-    console.log(`player lose, ${compSelection} beats ${playerSelection}`);
-    compScore++;
-    console.log(`player score: ${playerScore} computer score:${compScore}`);}
+    let sentenceResult = document.querySelector('.sentence-result');
+    let scoreResult = document.querySelector('.score-result');
+    let choiceResult = document.querySelector('.choice-result')
+    if (playerSelection == compSelection) {
+        choiceResult.textContent = `Player choose: ${playerSelection} | Computer choose: ${compSelection}`
+        sentenceResult.textContent = 'Round Result: It\'s a tie!'
+        scoreResult.textContent = `Player score: ${playerScore} | Computer score:${compScore}`
+    } else if (playerSelection == 'Rock' && compSelection == 'Scissor' ||
+              playerSelection == 'Scissor' && compSelection == 'Paper' ||
+              playerSelection == 'Paper' && compSelection == 'Rock') {
+        choiceResult.textContent = `Player choose: ${playerSelection} | Computer choose: ${compSelection}`
+        sentenceResult.textContent = `Round Result: Player win, ${playerSelection} beats ${compSelection}`
+        playerScore++;
+        scoreResult.textContent = `Player score: ${playerScore} | Computer score:${compScore}`
+    } else if (compSelection == 'Rock' && playerSelection == 'Scissor' ||
+              compSelection == 'Scissor' && playerSelection == 'Paper' ||
+              compSelection == 'Paper' && playerSelection == 'Rock') {
+        choiceResult.textContent = `Player choose: ${playerSelection} | Computer choose: ${compSelection}`
+        sentenceResult.textContent = `Round Result: Player lose, ${compSelection} beats ${playerSelection}`
+        compScore++;
+        scoreResult.textContent = `Player score: ${playerScore} | Computer score:${compScore}`
+    }
 };
 
-
-function play5Round(){
-    let keepGoing = true
-    while(keepGoing){
-        playRound();
-        if(playerScore >= 5 || compScore >= 5){
-            keepGoing = false;
-        }
-    }
-    if(playerScore == compScore){
-        console.log('tie!')
-    } else if(playerScore > compScore){
-        console.log('player Win!')
-    } else {
-        console.log('computer Win')
-    }
-    resetScore();
-};
-
+function playerSelect(){
+    const playerButtons = document.querySelectorAll('.button');
+    const endResult = document.querySelector('.end-result');
+    const playAgainButton = document.querySelector('.play-again');
+    playAgainButton.classList.add('hide-button')
+    playerButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            playerSelectValue = button.value;
+            playRound(playerSelectValue);    //playRound each time press button (bug over 2 hour) (dont put playRound outside this function, its async have to wait for input)
+            if(compScore >= 5 || playerScore >= 5){
+                showWinner();
+                playerButtons.forEach(button => { //have to use forEach cause its a NodeList
+                    button.classList.add('hide-button')});
+                playAgainButton.classList.remove('hide-button');
+                playAgain();
+            }
+        });
+    });
+}
 
 function resetScore(){
+    const endResult = document.querySelector('.end-result');
+    const sentenceResult = document.querySelector('.sentence-result');
+    const scoreResult = document.querySelector('.score-result');
     playerScore = 0;
     compScore = 0;
+    scoreResult.textContent = '';
+    endResult.textContent = '';
+    sentenceResult.textContent = '';
+ };
+
+
+function showWinner(){
+    const endResult = document.querySelector('.end-result');
+    const playerButtons = document.querySelectorAll('.button');
+    if(compScore > playerScore){
+        endResult.textContent = 'Computa Win!';
+    } else {
+        endResult.textContent = 'Player Win!';
+    } 
+    
 };
 
-
-// give comment for each score, ex 1 0 , yay u got 1 lead
-function currentScore(){
-    if
+function playAgain(){
+    const playerButtons = document.querySelectorAll('.button');
+    const playAgainButton = document.querySelector('.play-again');
+    
+    playAgainButton.addEventListener('click', () => {
+        playerButtons.forEach(button => {
+            button.classList.remove('hide-button');
+        });
+        playAgainButton.classList.add('hide-button');
+        resetScore(); //STACK EVENT IN ONE EVENTLISTENER
+    });
 }
+    
+//CAN STACK EVENT IN ONE EVENTLISTENER    
+//     playAgainButton.addEventListener('click', () => {
+//         playAgainButton.classList.add('hide-button')});
+        
+//     playAgainButton.addEventListener('click', resetScore);    
+// }
+
+function startGame(){
+
+}
+
+
+
+
+
+playerSelect();
